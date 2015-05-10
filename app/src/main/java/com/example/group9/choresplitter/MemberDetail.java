79304518a69701c82c52219fce4c41a6ce7c2543
Kevin Zhang <kzhang12@jhu.edu>
2015-05-10 15:06:03 -0400

    camera shit

--- app/src/main/java/com/example/group9/choresplitter/MemberDetail.java
+++ app/src/main/java/com/example/group9/choresplitter/MemberDetail.java
@@ -1,6 +1,9 @@
 package com.example.group9.choresplitter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,10 +12,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,11 +36,24 @@ public class MemberDetail extends ActionBarActivity {
     Context context;
     private List<Task> claimedTasks;
     private List<Task> completedTasks;
+    ImageView imgFavorite;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_detail);
+        Intent myIntent = getIntent(); // gets the previously created intent
+        username = myIntent.getStringExtra("name"); // will return "FirstKeyValue"
+        setTitle(username);
+
+        imgFavorite = (ImageView)findViewById(R.id.prof_pic);
+        imgFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open();
+            }
+        });
 
         //TODO: pull lists from database
         claimedTasks = new ArrayList<Task>();
@@ -43,6 +70,22 @@ public class MemberDetail extends ActionBarActivity {
         populateListView();
         registerClickCallbackClaimed();
         registerClickCallbackCompleted();
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        ParseQuery<ParseUser> userqueries = query.whereEqualTo("username", username);
+        try {
+            ParseUser user = userqueries.getFirst();
+            if (user.getParseFile("picture") != null)
+            {
+                ParseFile file = user.getParseFile("picture");
+                ImageView imageView = (ImageView) findViewById(R.id.prof_pic);
+                byte[] bitmapdata = file.getData();
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapdata, 0, bitmapdata.length);
+                imageView.setImageBitmap(bitmap);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -173,15 +216,53 @@ public class MemberDetail extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.credits:
+                Intent intent = new Intent(this, Credits.class);
+                startActivity(intent);
+                return true;
+            case R.id.camera:
+                open();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void open(){
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        imgFavorite.setImageBitmap(bp);
+        try {
+            saveImage(bp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        ImageView img= (ImageView) findViewById(R.id.prof_pic);
+        img.setImageBitmap(bp);
+    }
+
+    protected void saveImage(Bitmap bp) throws IOException, ParseException {
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        ParseFile file = new ParseFile("prof_pic.png", byteArray);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        ParseQuery<ParseUser> userqueries = query.whereEqualTo("username", username);
+        ParseUser user = userqueries.getFirst();
+        user.put("picture", file);
+        user.saveInBackground();
     }
 }
